99e1b4e4bfd486c0f1cf76bbfde0547aff95e482
FernandoJ17 <38330799+FernandoJ17@users.noreply.github.com>
2018-04-12 14:56:37 -0500

    Update projectoFlask.py

--- projectoFlask.py
+++ projectoFlask.py
@@ -1,8 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/') #wrap o decorador
-def index():
-    return "Hola mundo" #retorna un string
+@app.route('/user/<username>') #wrap o decorador
+def index(username):
+   return "Hola {} y bienvenido ".format(username)
 
-app.run()
+@app.route('/prime/<int:entero>') #wrap o decorador
+def primo(entero):
+    inicio = 1
+    while(inicio <= entero):
+        for i in range(2,inicio):
+            if (inicio%i)==0:
+                break
+        else:
+            return 'el numero {} es primo'.format(inicio)
+        inicio += 1
+
